11d2ac5712e5b3153fc7545a00412b84b057dfae
Igor Silva <igor.pereira@zup.com.br>
2021-10-29 10:25:20 -0300

    Piloto: Igor - Copiloto: Todos

--- src/test/java/br/com/zup/edu/pizzaria/ingredientes/cadastrodeingredientes/NovoIngredienteControllerTest.java
+++ src/test/java/br/com/zup/edu/pizzaria/ingredientes/cadastrodeingredientes/NovoIngredienteControllerTest.java
@@ -9,7 +9,10 @@ import java.math.BigDecimal;
 
 import javax.transaction.Transactional;
 
+import com.fasterxml.jackson.core.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,18 +31,64 @@ class NovoIngredienteControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    private final String URI = "/api/ingredientes";
+
     @Test
     void deveCadastrarNovoIngrediente() throws Exception {
 
         NovoIngredienteRequest body = new NovoIngredienteRequest("Queijo muçarela", new BigDecimal("2.0"), 200);
-        MockHttpServletRequestBuilder request = post("/api/ingredientes")
+        MockHttpServletRequestBuilder request = post(URI)
                                                 .contentType(MediaType.APPLICATION_JSON)
                                                 .content(new ObjectMapper().writeValueAsString(body));
-
         mvc.perform(request)
            .andExpect(status().isCreated())
            .andExpect(header().exists("Location"))
                 .andExpect(redirectedUrlPattern("/api/ingredientes/{spring:[0-9]+}"));
 
     }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void deveRetornarStatusCode400QuandoNomeInvalido(String input) throws Exception {
+        NovoIngredienteRequest body = new NovoIngredienteRequest(input, new BigDecimal("2.0"), 200);
+        MockHttpServletRequestBuilder request = post(URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(body));
+        mvc.perform(request)
+                .andExpect(status().isBadRequest());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "0"})
+    void deveRetornartatusCode400QuandoPrecoNegativoOuZero(String preco) throws Exception {
+        NovoIngredienteRequest body = new NovoIngredienteRequest("Queijo muçarela", new BigDecimal(preco), 200);
+        MockHttpServletRequestBuilder request = post(URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(body));
+        mvc.perform(request)
+                .andExpect(status().isBadRequest());
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void deveRetornarStatusCode400QuandoPrecoNulo(BigDecimal preco) throws Exception {
+        NovoIngredienteRequest body = new NovoIngredienteRequest("Queijo muçarela", preco, 200);
+        MockHttpServletRequestBuilder request = post(URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(body));
+        mvc.perform(request)
+                .andExpect(status().isBadRequest());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void deveRetornartatusCode400QuandoQuantidadeNegativaOuZero(int quantidade) throws Exception {
+        NovoIngredienteRequest body = new NovoIngredienteRequest("Queijo muçarela", new BigDecimal("2.0"), quantidade);
+        MockHttpServletRequestBuilder request = post(URI)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(body));
+        mvc.perform(request)
+                .andExpect(status().isBadRequest());
+    }
+
 }
